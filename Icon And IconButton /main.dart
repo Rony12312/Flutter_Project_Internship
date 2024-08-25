905723f910b08c0ea7b0c00491090267e1899c50
Rony12312 <128927848+Rony12312@users.noreply.github.com>
2024-08-25 23:34:11 +0300

    Update Icon And IconButton Project

--- Icon And IconButton /main.dart
+++ Icon And IconButton /main.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.blue,
+              title: const Text("Icon And IconButton"),
+            ),
+            body: Container(
+                child: IconButton(
+              iconSize: 200,
+              onPressed: () {
+                print("hello");
+              },
+              icon: Icon(Icons.star, color: Colors.blue),
+            ))));
+  }
+}
 
