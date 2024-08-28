c966c3e4960901616c2f5bb165197079a9d42051
Rony12312 <128927848+Rony12312@users.noreply.github.com>
2024-08-28 22:38:18 +0300

    Create Icon Button Project

---
+++ Buttons/Icon Button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.blue,
+              title: const Text("Example App"),
+            ),
+            body: Container(
+              child:IconButton(onPressed: (){
+                print("hello world");
+              }, icon: Icon(Icons.person, color: Colors.blue, size: 100),
+
+
+            ))));
+  }
+}
