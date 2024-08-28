8b79e90cbf27351367e1be7cd7248b0712180fd5
Rony12312 <128927848+Rony12312@users.noreply.github.com>
2024-08-28 23:01:59 +0300

    Update Icon Button Project

--- Buttons/Icon Button.dart
+++ Buttons/Icon Button.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
             appBar: AppBar(
               backgroundColor: Colors.blue,
-              title: const Text("Example App"),
+              title: const Text("Icon Button"),
             ),
             body: Container(
               child:IconButton(onPressed: (){
