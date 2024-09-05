643925122c2e1a80a4196d71a6dc8cff700c86d4
Rony12312 <128927848+Rony12312@users.noreply.github.com>
2024-09-06 00:54:16 +0300

    Create homepage.dart

---
+++ Navigator/homepage.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:untitled7/aboutus.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Home Page",
+          style: TextStyle(fontSize: 20),
+        ),
+        backgroundColor: Colors.blue,
+        titleTextStyle: const TextStyle(color: Colors.white),
+      ),
+      body: ListView(
+        children: [
+          Center(
+              child: Text(
+            "Home Page",
+            style: TextStyle(fontSize: 30),
+            textAlign: TextAlign.center,
+          )),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: MaterialButton(
+              color: Colors.blue,
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AboutUs()));
+              },
+              child: Text("Go to About Us"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
